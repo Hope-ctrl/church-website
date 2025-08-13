02a881a70f3bbcb7da86e32f4a628c2eada7af44
Hope-ctrl <adebara432@gmail.com>
2025-08-12 22:22:48 -0700

    router

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/church-website">
     <App />
     </BrowserRouter>
   </StrictMode>,
